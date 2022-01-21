7d9f2c51636c8c3839f49740b1fc9180899637f9
Temidayo Ajisebutu <dayoajisebutu@gmail.com>
2022-01-21 10:46:22 +0100

    both websocket and rest working accordingly

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const port = 3010;
+const path = require('path');
+const axios = require('axios');
+
+const expressWs = require('express-ws')(express());
+const app = expressWs.app;
+
+
+app.use(express.static('static'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('pages/index.html'));
+});
+
+app.post('/uniqueElements', (req, res) => {
+  const arr = req.body.arr;
+  const uniqueElements = uniqueValues(arr);
+  res.json({ status: 1, uniqueElements });
+});
+
+const uniqueValues = (arr) => {
+  let count = arr.reduce((a, b) => ({ ...a, [b]: (a[b] || 0) + 1 }), {});
+  return Object.keys(count).filter((a) => count[a] === 1);
+};
+
+app.ws('/coins', function (ws, req) {
+  setInterval(async () => {
+    const payload = await getCoinMarketInfo();
+    ws.send(JSON.stringify(payload));
+  }, (3 * 60 * 1000));
+});
+
+app.ws('/', function (ws, req) {
+  console.log('inside');
+  ws.on('message', function (msg) {
+    console.log(msg);
+  });
+  console.log('socket', req.testing);
+  ws.send('Hello Express');
+});
+
+const getCoinMarketInfo = async () => {
+  try {
+    const payload = await axios.get(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    );
+    return payload.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
